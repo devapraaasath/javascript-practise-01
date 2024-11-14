d9f79e0073ad15662f8c7ce90d3356dca76f053a
Devaprasath m <144353578+devapraaasath@users.noreply.github.com>
2024-11-15 00:38:11 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+function sample1() {
+    console.log('test1');
+    document.getElementById('demo').innerHTML = 'morning 9 am to 11 am';
+}
+function sample2() {
+    console.log('test2');
+    document.getElementById('demo').innerHTML = 'evening 6pm to 8pm';
+}
+function view() {
+    console.log('test3'); // Corrected typo here
+    document.getElementById('vegeta').src = "images/2706a0e7-ef6d-4d69-9861-9bce383d9fe1.jpeg";
+}
+function img1(){
+    console.log('test4');
+    document.getElementById('img1').src = "images/surya1.jpg" ;
+}
+function img2(){
+    console.log('test5');
+    document.getElementById('img2').src = "images/img-2.jfif" ;
+}
+function img3(){
+    console.log('test6');
+    document.getElementById('img3').src = "images/joker.jfif" ;
+}
+function img4(){
+    console.log('test7');
+    document.getElementById('img4').src = "images/batman.jfif" ;
+}
+function img5(){
+    console.log('test7');
+    document.getElementById('img5').src = "images/superman.jfif" ;
+}
